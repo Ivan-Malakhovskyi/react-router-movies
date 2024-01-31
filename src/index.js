72e39671364a0bec6ff11be027d200522d5d124f
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2024-01-31 12:16:05 +0200

    updated name repo

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ const theme = {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/goit-react-hw-05-movies">
+      <BrowserRouter basename="/react-router-movies">
         <App />
       </BrowserRouter>
     </ThemeProvider>
